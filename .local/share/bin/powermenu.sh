1c141bd23f9f05492dc9c8704e8b25f43a2f2d18
harsha <pharshavardhan2@outlook.com>
2024-08-19 03:08:34 +0530

    add nautilus geeqie

--- .local/share/bin/powermenu.sh
+++ .local/share/bin/powermenu.sh
@@ -5,7 +5,7 @@
 CHOSEN=$(printf "Lock\nSuspend\nReboot\nShutdown\nLog Out" | tofi --config $XDG_CONFIG_HOME/tofi/powermenu_config)
 
 case "$CHOSEN" in
-	"Lock") playerctl pause && loginctl lock-session ;;
+	"Lock") playerctl pause; loginctl lock-session ;;
 	"Suspend") systemctl suspend-then-hibernate ;;
 	"Reboot") systemctl reboot ;;
 	"Shutdown") systemctl poweroff ;;
